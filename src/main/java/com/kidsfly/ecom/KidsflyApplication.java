c194f8ec8e0e8f57029fc896cfee71fd3260725f
Krishna <sairamkrishna1412@gmail.com>
2024-03-18 20:02:44 +0530

    Init commit

---
+++ src/main/java/com/kidsfly/ecom/KidsflyApplication.java
@@ -0,0 +1,13 @@
+package com.kidsfly.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidsflyApplication.class, args);
+	}
+
+}
